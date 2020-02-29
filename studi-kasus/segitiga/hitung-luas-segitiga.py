66544cfec2f3edbadcd3a9cbf27b16cca80a1642
Yurimahendra <yurimahendra40@gmail.com>
2020-02-29 15:53:35 +0700

    menghitung luas segitiga dengan jumlah segitiga sebanyak 123

---
+++ studi-kasus/segitiga/hitung-luas-segitiga.py
@@ -0,0 +1,12 @@
+# jumlah segitiga
+n = 123
+# panjang alas sebuah segitiga
+alas = 30
+# tinggi sebuah segitiga
+tinggi = 18
+# hitung luas sebuah segitiga
+luas = alas * tinggi * 1/2
+# hitung luas total
+luastotal = n * luas
+
+print('luas total : ', luastotal,'satuan luas')
