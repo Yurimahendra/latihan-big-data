f72fdca5dfb817ba809a316ceb4c651aa6def010
Yurimahendra <yurimahendra40@gmail.com>
2020-03-05 09:43:56 +0700

    menghitung keramik cadangan

---
+++ studi-kasus/keramik-cadangan/hitung-keramik-cadangan.py
@@ -0,0 +1,12 @@
+# jumlah keramik
+JumKeramik = 533
+#jumlah box
+jumBox = 106
+# hitung keramik cadangan 10% dari 533
+keramikCad = 10 * 533 / 100
+#total box yang diperlukan
+totBox = jumBox + keramikCad
+print("total box keramik yang diperlukan = ", totBox)
+
+
+
