283a1288dafc08d41da56de377f1db51e169d730
Yurimahendra <yurimahendra40@gmail.com>
2020-03-05 09:16:28 +0700

    menghitung box keramik

---
+++ studi-kasus/box-keramik/jumlah-box-keramik.py
@@ -0,0 +1,9 @@
+# jumlah keramik
+JumKeramik = 533
+# jumlah 1 box keramik adalah 5
+box = 5
+#hitung total box keramik yang dibutuhkan
+totalbox = JumKeramik / box
+print("jumlah box yang diperlukan = ", totalbox)
+
+
