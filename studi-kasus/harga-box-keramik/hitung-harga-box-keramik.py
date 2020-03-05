a79711837c5c263fcf90e55c3f7c9746b1874a0c
Yurimahendra <yurimahendra40@gmail.com>
2020-03-05 09:56:11 +0700

    menghitung biaya pembelian keramik

---
+++ studi-kasus/harga-box-keramik/hitung-harga-box-keramik.py
@@ -0,0 +1,7 @@
+#jumlah box keramik yang diperlukan
+jumbox= 159
+#harga per box
+hargabox = 125000
+#hitung biaya yang diperlukan untuk membeli 159 box keramik
+totharga = jumbox * hargabox
+print("total biaya yang diperlukan Rp.", totharga)
