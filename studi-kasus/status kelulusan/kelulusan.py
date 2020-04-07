d51e58ff7f2f38c3fb76127c3039589dc069bbcd
Yurimahendra <yurimahendra40@gmail.com>
2020-04-07 17:19:06 +0700

    menambahkan sebab ketidaklulusan

--- studi-kasus/status kelulusan/kelulusan.py
+++ studi-kasus/status kelulusan/kelulusan.py
@@ -7,6 +7,31 @@ if indo >= 0 and indo <=100 and ipa >= 0 and ipa <= 100 and mtk >= 0 and mtk <=1
         hasil = 'lulus'
     elif indo < 60 or ipa < 60 or mtk <= 70 :
         hasil = 'tidak lulus'
+
     print('status : ', hasil)
+
 else:
     print('nilai tidak validd')
+
+def sebab():
+    if indo < 60 and ipa < 60 and mtk <=70:
+       print('nilai ipa kurang dari 60')
+       print('nilai bahasa indonesia kurang dari 60' )
+       print('nilai matematika kurang dari atau sama dengan 70')
+    elif indo < 60 and ipa < 60:
+       print('nilai bahasa indonesia kurang dari 60')
+       print('nilai ipa kurang dari 60')
+    elif indo < 60 and mtk <= 70:
+       print('nilai bahasa indonesia kurang dari 60')
+       print('nilai matematika kurang dari atau sama dengan 70')
+    elif ipa < 60 and mtk <= 70:
+       print('nilai bahasa indonesia kurang dari 60')
+       print('nilai matematika kurang dari atau sama dengan 70')
+    elif mtk <= 70:
+        print('nilai matematika kurang dari atau sama dengan 70')
+    elif indo < 60 :
+        print('nilai bahasa indonesia kurang dari 60')
+    elif ipa < 60:
+        print('nilai ipa kurang dari 60')
+
+sebab()
